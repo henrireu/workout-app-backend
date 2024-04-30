e5c46d0ebd92b161745ae7e1cb3d6f4feecbc9a9
reijomo <95440863+reijomo@users.noreply.github.com>
2024-04-30 11:40:24 +0300

    p

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
